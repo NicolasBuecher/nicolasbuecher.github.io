acfe316fef0f9e395c01cae0cc14be15834abce1
nicolasb <buecher.nicolas@gmail.com>
2022-07-28 21:17:25 +0200

    feat: Complete Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
+import { Reorder } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button></button>
+        <button onClick={() => {setExpandNavbar((prev) => !prev);}}>
+          <Reorder />
+        </button>
       </div>
       <div className="links">
         <Link to="/"> Home </Link>
