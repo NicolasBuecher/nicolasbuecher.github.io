f58a5686330da2c3181d95e341443636e803e8c3
nicolasb <buecher.nicolas@gmail.com>
2022-08-03 03:20:53 +0200

    feat: Create Experience page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import Experience from "./pages/Experience";
+import Experience from "./pages/Experience/Experience";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects";
 
--- src/pages/Experience.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-function Experience(): ReactElement {
-  return (
-    <div>Experience</div>
-  );
-}
-
-export default Experience;
